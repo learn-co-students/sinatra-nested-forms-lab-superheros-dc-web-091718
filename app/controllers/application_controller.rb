22bee03ac129eb34bf8eaf35bf8839966811f9e6
nachoespino53 <nachoespino53@gmail.com>
2018-10-09 12:25:33 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get "/" do
+      erb :super_hero
+    end
+
+    get "/teams" do
+      erb :team
     end
 
 end
